78075cb8b97eb8f69c422d07eface1aec6d9ef3e
harshsingh <harsh220902@gmail.com>
2025-04-13 22:51:01 +0530

    Custom data types

--- fitness-team/agent_team.py
+++ fitness-team/agent_team.py
@@ -2,6 +2,46 @@ from agents import (
     Agent, Runner, function_tool
 )
 
+from pydantic import BaseModel
+from enum import Enum
 
-# Multi Agent Collab
+###############################
+## Multi Agent Collaboration ##
+###############################
+
+
+# Defining Custom types
+
+class UserFitnessProfile(BaseModel):
+    name: str
+    age: int
+    weight: int
+    height: float
+    food_preference: str
+    fitness_goal: str
+    
+class DietPlan(BaseModel):
+    description: str
+    target_calories: int
+    protien_target: int
+    breakfast: str
+    lunch: str
+    dinner: str
+    
+class IntensityLevel(Enum):
+    Low = "Low"
+    Medium = "Medium"
+    High = "High"
+    
+class WorkoutPlan(BaseModel):
+    description: str
+    intensity: IntensityLevel
+    frequency: int
+    full_workout_plan: str
+    
+class Report(BaseModel):
+    user_fitness_profile: UserFitnessProfile | None
+    diet_plan: DietPlan | None
+    workout_plan: WorkoutPlan | None
+    
 
